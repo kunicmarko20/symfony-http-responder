3681e308981adaf6efd759423ec5afd0cc134d5e
kunicmarko20 <kunicmarko20@gmail.com>
2019-03-06 20:01:47 +0000

    FlashResponder

---
+++ src/FlashResponderDecorator.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ro0NL\HttpResponder;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+
+/**
+ * @author Marko Kunic <kunicmarko20@gmail.com>
+ */
+final class FlashResponderDecorator implements Responder
+{
+    /**
+     * @var Responder
+     */
+    private $responder;
+
+    /**
+     * @var FlashBagInterface
+     */
+    private $flashBag;
+
+    public function __construct(Responder $responder, FlashBagInterface $flashBag)
+    {
+        $this->responder = $responder;
+        $this->flashBag = $flashBag;
+    }
+
+    public function respond(Respond $respond): Response
+    {
+        foreach ($respond->flashes as $type => $messages) {
+            foreach ((array) $messages as $message) {
+                $this->flashBag->add($type, $message);
+            }
+        }
+
+        return $this->responder->respond($respond);
+    }
+}
