3ecc89787623d0ac89b203c413e910cab42a5dae
kunicmarko20 <kunicmarko20@gmail.com>
2019-03-06 20:09:00 +0000

    Symfony Serializer Responder

---
+++ src/Bridge/Serializer/RespondSerializer.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ro0NL\HttpResponder\Bridge\Serializer;
+
+use ro0NL\HttpResponder\Respond;
+
+/**
+ * @author Marko Kunic <kunicmarko20@gmail.com>
+ */
+final class RespondSerializer extends Respond
+{
+    /**
+     * @var mixed
+     */
+    public $data;
+
+    /**
+     * @var string
+     */
+    public $format;
+
+    /**
+     * @var array
+     */
+    public $context;
+
+    public function __construct($data, string $format, array $context = [])
+    {
+        $this->data = $data;
+        $this->format = $format;
+        $this->context = $context;
+    }
+}
---
+++ src/Bridge/Serializer/SerializerResponder.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ro0NL\HttpResponder\Bridge\Serializer;
+
+use ro0NL\HttpResponder\Exception\BadRespondTypeException;
+use ro0NL\HttpResponder\Respond;
+use ro0NL\HttpResponder\Responder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @author Marko Kunic <kunicmarko20@gmail.com>
+ */
+final class SerializerResponder implements Responder
+{
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public function respond(Respond $respond): Response
+    {
+        if ($respond instanceof RespondSerializer) {
+            return new Response(
+                $this->serializer->serialize(
+                    $respond->data,
+                    $respond->format,
+                    $respond->context
+                ),
+                $respond->status,
+                $respond->headers
+            );
+        }
+
+        throw BadRespondTypeException::create($this, $respond);
+    }
+}
